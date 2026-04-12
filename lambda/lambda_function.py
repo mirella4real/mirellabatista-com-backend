4b7ced0b4a8172bd39e5fe108e8fed5532677727
Mirella Batista <mirelbatista@gmail.com>
2026-04-12 15:04:17 -0400

    feat: add visitor counter Lambda function

---
+++ lambda/lambda_function.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ.get("DYNAMODB_TABLE", "visitor-counter"))
+
+
+def lambda_handler(event, context):
+    response = table.update_item(
+        Key={"id": "visitor_count"},
+        UpdateExpression="ADD #v :increment",
+        ExpressionAttributeNames={"#v": "views"},
+        ExpressionAttributeValues={":increment": 1},
+        ReturnValues="UPDATED_NEW",
+    )
+    count = int(response["Attributes"]["views"])
+    return {"statusCode": 200, "body": json.dumps({"views": count})}
